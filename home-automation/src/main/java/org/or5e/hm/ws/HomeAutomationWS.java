04139700ffb6641d95f003083ac3abfaee757169
Ponraj Suthanthiramani <mylife.ponraj@gmail.com>
2018-03-26 16:47:05 +0530

    new code

--- home-automation/src/main/java/org/or5e/hm/ws/HomeAutomationWS.java
+++ home-automation/src/main/java/org/or5e/hm/ws/HomeAutomationWS.java
@@ -5,23 +5,43 @@ import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/hmautomation")
+import org.or5e.hm.sm.MasterUnitController;
+import org.or5e.hm.ws.message.Message;
+import org.or5e.hm.ws.message.MessageDecoder;
+import org.or5e.hm.ws.message.MessageEncoder;
+
+@ServerEndpoint(
+		value="/hmautomation/{user}",
+		decoders = MessageDecoder.class,
+		encoders = MessageEncoder.class
+		)
 public class HomeAutomationWS {
+	private Session session;
+	private String user;
+	private MasterUnitController _controller;
+	public HomeAutomationWS() {
+		_controller = MasterUnitController.getController();
+	}
 
 	@OnOpen
-	public void onOpen(Session session) {
-		System.out.println("Open Connection ..."+session.getId());
+	public void onOpen(@PathParam("user") String user, Session session) {
+		System.out.println("Open Connection ..."+user);
+		this.user = user;
+		this.session = session;
+		_controller.addUser(user, session);
 	}
 
 	@OnClose
-	public void onClose(Session session) {
+	public void onClose(@PathParam("user") String user, Session session) {
 		System.out.println("Close Connection ..."+session.getId());
+		_controller.removeUser(user);
 	}
 
 	@OnMessage
-	public String onMessage(String message, Session session) {
+	public String onMessage(Message message, Session session) {
 		System.out.println("Message from the client: " + message + " From: "+session.getId());
 		String echoMsg = "Echo from the server : " + message;
 		return echoMsg;
@@ -30,5 +50,10 @@ public class HomeAutomationWS {
 	@OnError
 	public void onError(Throwable e) {
 		e.printStackTrace();
+		System.out.println("User disconnected: "+this.user);
+	}
+
+	public static void main(String[] args) {
+		new HomeAutomationWS();
 	}
 }
